915b82ceeed60e3f6ed9eda7ce02883d0658c746
Ilya <86313716+eqodel@users.noreply.github.com>
2022-04-03 12:55:24 +0300

    create responsive

--- js/main.js
+++ js/main.js
@@ -19,3 +19,22 @@ const swiper = new Swiper('.swiper', {
       }
     }
   });
+
+
+  var menuButton = $('.menu__button');
+  menuButton.on('click', function () {
+    $('.header-wrapper').toggleClass('header-wrapper--mobile');
+    $('.header__menu').toggleClass('header__menu--mobile');
+    $('.header__logo').toggleClass('header__logo--mobile');
+    $('.header__social').toggleClass('header__social--mobile');
+    $('.menu__close').toggleClass('menu__close--mobile');
+  });
+
+  var closeButton = $('.menu__close');
+  closeButton.on('click', function () {
+    $('.header-wrapper').toggleClass('header-wrapper--mobile');
+    $('.header__menu').toggleClass('header__menu--mobile');
+    $('.header__logo').toggleClass('header__logo--mobile');
+    $('.header__social').toggleClass('header__social--mobile');
+    $('.menu__close').toggleClass('menu__close--mobile');
+  });
